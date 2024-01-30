769d592ec618e6e06dbc2e4a020d73ef001b13ec
S10-Original <stenisamberth@gmail.com>
2024-01-30 11:28:04 -0400

    avance pagina miembros
    avance

--- src/ventanas/Miembros.js
+++ src/ventanas/Miembros.js
@@ -3,8 +3,21 @@ import '../css/cssPrincipal.css';
 
 function Miembros() {
     return(
-        <div>
-            <h1>Miembros</h1>
+        <div className="formatoPagina">
+            <div className="Botonera">
+                <div className="BotoneraIzquierda">
+                    <input ></input>
+                    <button>Buscar</button>
+                </div>
+                <div className="BotoneraDerecha">
+                    <button>Nuevo Registro</button>
+                    <button>Reporte PDF</button>
+                </div>
+            </div>
+            
+            <div className="formatoTabla">
+                <p>prueba</p>
+            </div>
         </div>
     );
 }
